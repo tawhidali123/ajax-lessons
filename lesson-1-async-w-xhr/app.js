9ed8e2c122ad17b8699fcd1c3df985f7ffc91023
TAWHID ALI <tawhidali@TAWHIDs-MacBook-Pro.local>
2019-04-29 21:45:35 -0400

    unsplash Get request

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -10,3 +10,16 @@
         searchedForText = searchField.value;
     });
 })();
+
+
+const unsplashRequest = new XMLHttpRequest();
+
+const searchedForText = 'hippos';
+unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+unsplashRequest.onload = addImage;
+unsplashRequest.setRequestHeader('Authorization', 'Client-ID 3fa69d468a0ea3881fa29f9dbe0228c45cba9aafb2a479b51200c1d7acfd8d1e');
+unsplashRequest.send();
+
+function addImage(){
+    debugger;
+}
